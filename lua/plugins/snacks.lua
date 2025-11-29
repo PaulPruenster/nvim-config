3969576d9c456f8fbff9365a13f4dd7914971719
paulpruenster <paul.pruenster@gmail.com>
2025-11-29 22:07:03 +0100

    open and close folders with arrows

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,8 +7,9 @@ return {
           keys = {
             -- Map single left mouse click to "confirm" (open file)
             ["<LeftRelease>"] = "confirm",
-            -- Optional: You can also remove the double-click mapping if you wish
-            -- ["<2-LeftMouse>"] = nil,
+
+            ["<Right>"] = "confirm",
+            ["<Left>"] = "explorer_up",
           },
         },
       },
