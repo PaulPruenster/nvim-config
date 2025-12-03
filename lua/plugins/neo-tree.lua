754beb2906fcd0d7046697dccf75c1a13bf85a5c
paulpruenster <paul.pruenster@gmail.com>
2025-12-03 20:22:26 +0100

    switch from snacks explorer to neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+-- File: ~/.config/nvim/lua/plugins/neo-tree.lua
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      mappings = {
+        -- Override the default 'h' key to close a folder
+        ["h"] = "close_node",
+        ["<Left>"] = "close_node", -- New: left arrow also closes
+        -- Override the default 'l' key to open a file/folder
+        ["l"] = "open",
+        ["<Right>"] = "open", -- New: right arrow also opens
+        -- Optional: Keep other useful default bindings visible
+        ["<space>"] = "none", -- Prevents space from toggling selection
+      },
+    },
+  },
+}
