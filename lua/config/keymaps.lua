f502039555b1afa721b2e9061f272a1c5ca2ee43
paulpruenster <paul.pruenster@gmail.com>
2025-12-03 22:10:15 +0100

    add global window change shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,3 +12,10 @@ vim.keymap.set("n", "<leader>q", ":qa<CR>", { desc = "Quit All" })
 vim.keymap.set("n", "<C-j>", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal", noremap = true, silent = true })
 -- Optional: Also make it work in insert mode
 vim.keymap.set("i", "<C-j>", "<Esc><cmd>ToggleTerm<CR>", { desc = "Toggle terminal", noremap = true, silent = true })
+
+-- These are 'global' keymaps, active in Normal, Insert, and Terminal modes.
+vim.keymap.set({ "n", "i", "t" }, "<A-s>", "<C-w>h", { desc = "Focus window left" })
+vim.keymap.set({ "n", "i", "t" }, "<A-f>", "<C-w>l", { desc = "Focus window left" })
+
+vim.keymap.set({ "n", "i", "t" }, "<A-e>", "<C-w>k", { desc = "Focus window up" })
+vim.keymap.set({ "n", "i", "t" }, "<A-d>", "<C-w>j", { desc = "Focus window down" })
