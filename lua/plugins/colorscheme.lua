dac976af4404770d6e67407d076dd0f111f743be
paulpruenster <paul.pruenster@gmail.com>
2025-12-09 21:02:20 +0100

    switch back to molokai

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,32 +1,32 @@
-return {
-  -- Add the Sonokai theme plugin
-  {
-    "sainnhe/sonokai",
-    init = function()
-      -- Set the variant before the theme is loaded
-      vim.g.sonokai_style = "andromeda"
-    end,
-  },
-  -- Configure LazyVim to load this colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "sonokai",
-    },
-  },
-}
-
 -- return {
+--   -- Add the Sonokai theme plugin
 --   {
---     "khoido2003/monokai-v2.nvim",
---     priority = 1000,
---     lazy = false,
---     config = function()
---       require("monokai-v2").setup({
---         filter = "spectrum",
---         terminal_colors = true,
---       })
---       vim.cmd("colorscheme monokai-v2")
+--     "sainnhe/sonokai",
+--     init = function()
+--       -- Set the variant before the theme is loaded
+--       vim.g.sonokai_style = "andromeda"
 --     end,
 --   },
+--   -- Configure LazyVim to load this colorscheme
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "sonokai",
+--     },
+--   },
 -- }
+
+return {
+  {
+    "khoido2003/monokai-v2.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require("monokai-v2").setup({
+        filter = "spectrum",
+        terminal_colors = true,
+      })
+      vim.cmd("colorscheme monokai-v2")
+    end,
+  },
+}
