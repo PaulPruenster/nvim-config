556874559bb6467e157aa6c282b8109f10dbb4ea
paulpruenster <paul.pruenster@gmail.com>
2026-08-08 20:34:00 +0200

    new floating terminal ctrl k

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,10 +8,16 @@ vim.keymap.set("i", "<C-a>", "<Esc>ggVG", { desc = "Select entire file" })
 
 vim.keymap.set("n", "<leader>q", ":qa<CR>", { desc = "Quit All" })
 
--- Map Ctrl-j to toggle the terminal
-vim.keymap.set("n", "<C-j>", "<cmd>ToggleTerm<CR>", { desc = "Toggle terminal", noremap = true, silent = true })
+-- Map Ctrl-j to toggle the horizontal terminal (id 1)
+vim.keymap.set("n", "<C-j>", "<cmd>1ToggleTerm<CR>", { desc = "Toggle terminal", noremap = true, silent = true })
 -- Optional: Also make it work in insert mode
-vim.keymap.set("i", "<C-j>", "<Esc><cmd>ToggleTerm<CR>", { desc = "Toggle terminal", noremap = true, silent = true })
+vim.keymap.set("i", "<C-j>", "<Esc><cmd>1ToggleTerm<CR>", { desc = "Toggle terminal", noremap = true, silent = true })
+-- Close the terminal while inside it
+vim.keymap.set("t", "<C-j>", "<cmd>ToggleTerm<CR>", { desc = "Close terminal", noremap = true, silent = true })
+
+-- Toggle floating terminal (like a lazygit popup), keeps its state
+vim.keymap.set("n", "<C-k>", "<cmd>2ToggleTerm direction=float<CR>", { desc = "Toggle float terminal", noremap = true, silent = true })
+vim.keymap.set("t", "<C-k>", "<cmd>2ToggleTerm<CR>", { desc = "Toggle float terminal", noremap = true, silent = true })
 
 -- These are 'global' keymaps, active in Normal, Insert, and Terminal modes.
 vim.keymap.set({ "n", "i", "t" }, "<A-s>", "<C-w>h", { desc = "Focus window left" })
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,12 +5,15 @@ return {
     opts = {
       -- Your configuration options go here
       size = 20,
-      -- Set the default open mapping to Ctrl-j
-      open_mapping = [[<c-j>]],
       direction = "horizontal", -- or "float", "vertical"
       start_in_insert = true,
       persist_size = true,
       close_on_exit = true,
+      float_opts = {
+        border = "curved",
+        title_pos = "center",
+        winblend = 0,
+      },
       -- Add other options as needed
     },
   },
