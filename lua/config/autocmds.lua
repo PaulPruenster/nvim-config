2784a45ae0874b2900c06527e1334b61e6b6826e
paulpruenster <paul.pruenster@gmail.com>
2026-08-29 18:58:00 +0200

    opencode on startup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,23 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local Terminal = require("toggleterm.terminal").Terminal
+    Terminal:new({
+      id = 2,
+      cmd = "opencode",
+      direction = "float",
+      dir = vim.fn.getcwd(),
+      hidden = true,
+      close_on_exit = true,
+      float_opts = {
+        border = "curved",
+        title_pos = "center",
+        winblend = 0,
+      },
+    }):spawn()
+  end,
+  once = true,
+})
